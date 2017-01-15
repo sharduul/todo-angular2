3ab45fce02ca09a0001130f292fbe3b97a485a30
Shardul <bagadeshardul@gmail.com>
2017-01-14 20:07:31 -0500

    firebase hookedup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(){
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyARhXidpvymn3AGsmDWmd43lShiqX_MLec",
+      authDomain: "todo-angular2-f073e.firebaseapp.com",
+      databaseURL: "https://todo-angular2-f073e.firebaseio.com",
+      storageBucket: "todo-angular2-f073e.appspot.com",
+      messagingSenderId: "224605495324"
+    };
+    initializeApp(config);
+
+    var root = database().ref('messages/1');
+    root.on('value', function(snapshot){
+      console.log(snapshot.key, snapshot.val());
+    });
+
+  }
 }
