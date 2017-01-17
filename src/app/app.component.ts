d6c0ecd0c81b28e09a8209846b52224e035f2937
Shardul <bagadeshardul@gmail.com>
2017-01-16 20:50:44 -0500

    install angularfire2 lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { initializeApp, database } from 'firebase';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -9,21 +11,39 @@ import { initializeApp, database } from 'firebase';
 export class AppComponent {
   title = 'app works!';
 
-  constructor(){
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyARhXidpvymn3AGsmDWmd43lShiqX_MLec",
-      authDomain: "todo-angular2-f073e.firebaseapp.com",
-      databaseURL: "https://todo-angular2-f073e.firebaseio.com",
-      storageBucket: "todo-angular2-f073e.appspot.com",
-      messagingSenderId: "224605495324"
-    };
-    initializeApp(config);
+  constructor(private af: AngularFire){
 
-    var root = database().ref('messages/1');
-    root.on('value', function(snapshot){
-      console.log(snapshot.key, snapshot.val());
-    });
+
+    const tasks$ : FirebaseListObservable<any> = af.database.list('tasks');
+
+    tasks$.subscribe(
+
+      val => console.log(val)
+
+    );
+
+
+
+    // NOTE: kept here for reference. In case you are using firebase sdk directly.
+    //// Initialize Firebase
+    //var config = {
+    //  apiKey: "AIzaSyARhXidpvymn3AGsmDWmd43lShiqX_MLec",
+    //  authDomain: "todo-angular2-f073e.firebaseapp.com",
+    //  databaseURL: "https://todo-angular2-f073e.firebaseio.com",
+    //  storageBucket: "todo-angular2-f073e.appspot.com",
+    //  messagingSenderId: "224605495324"
+    //};
+    //initializeApp(firebaseConfig);
+    //
+
+    // You don't need this if you are using angularfire2 library
+    // angularfire2 provides great wrapper functions for the firebase sdk
+    //var root = database().ref('messages/1');
+    //root.on('value', function(snapshot){
+    //  console.log(snapshot.key, snapshot.val());
+    //});
+
+
 
   }
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyARhXidpvymn3AGsmDWmd43lShiqX_MLec",
+  authDomain: "todo-angular2-f073e.firebaseapp.com",
+  databaseURL: "https://todo-angular2-f073e.firebaseio.com",
+  storageBucket: "todo-angular2-f073e.appspot.com",
+  messagingSenderId: "224605495324"
+};
